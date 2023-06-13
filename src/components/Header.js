38a0737268acada9c14ab5e435e7e54b54d045fb
jacobbarnette <jacobcbarnette@gmail.com>
2023-06-13 16:38:25 -0400

    added contact info

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,31 +1,36 @@
+import { FiPhone } from "react-icons/fi";
 import Link from "next/link";
 import styles from "../styles/Header.module.css";
+
 const Header = () => {
   return (
     <header className={styles.header}>
-      <div className={styles.logo}>
-        <h1>Knox Roofing</h1>
+      <div className={styles.horizontalBar}>
+        <span className={styles.spacer}></span>
+        <div className={styles.redBar}>
+          <FiPhone className={styles.phone} />{" "}
+          <span className={styles.phone}>Phone: 812-344-9836</span>
+          <span className={styles.spacer}></span>
+          Contact US!
+        </div>
       </div>
-      <nav>
-        <ul>
-          <li>
+
+      <nav className={styles.nav}>
+        <ul className={styles.navItems}>
+          <li className={styles.navItem}>
             <Link href="/">Home</Link>
           </li>
-          <li>
-            <Link href="/services">Services </Link>
+          <li className={styles.navItem}>
+            <Link href="/services">Services</Link>
           </li>
-          <li>
+          <li className={styles.navItem}>
             <Link href="/about">About</Link>
           </li>
-          <li>
+          <li className={styles.navItem}>
             <Link href="/contact">Contact</Link>
           </li>
         </ul>
       </nav>
-      <div className={styles.ribbon}>
-        <span>Contact us:</span>
-        <span>123-456-7890</span>
-      </div>
     </header>
   );
 };
