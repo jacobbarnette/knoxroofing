a8b54ac01309c00d7caeb3fbf9a591cb155e9ead
jacobbarnette <jacobcbarnette@gmail.com>
2023-06-22 15:11:55 -0400

    updated form

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,13 @@
 import Image from 'next/image'
-
+import About from '../components/About'
+import ImageWithForm from '../components/ImageWithForm'
 import Navbar from '../components/Navbar'
 export default function Home() {
   return (
     <div>
-      <Navbar 
-        link1='About Us'
-        link1To='/about'
-        link2='Contact Us'
-        link2To='/contact'
-        link3='Subscribe'
-        link3To='/subscribe'/>
+      <Navbar/>
+      <ImageWithForm/>
+      <About />
     </div>
       )
 }
