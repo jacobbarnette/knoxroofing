0896ff96c394fc84b09f06960ec353de4da0f4c4
jacobbarnette <jacobcbarnette@gmail.com>
2023-06-27 12:19:41 -0400

    increased navbar size and font size

--- src/components/ImageWithForm.js
+++ src/components/ImageWithForm.js
@@ -1,16 +1,21 @@
 import React from "react";
 import RoofingForm from "./RoofingForm";
 import Image from "next/image";
+import styles from "../styles/Navbar.module.css";
+import Tagline from "./Tagline";
 const ImageWithForm = () => {
   return (
     <div className="relative">
-      <Image
-        src="https://media.istockphoto.com/id/1334926091/photo/roof-of-new-house-with-shingles-roof-tiles-and-ventilation-window.jpg?s=2048x2048&w=is&k=20&c=N9WMvRaYjmgo3LUF4UelcLmxCH1hj7RxMZK5ER8LPx8="
-        className="h-full w-full"
-        alt="Your Image"
-        width={500}
-        height={500}
-      />
+      <div className={styles.imageContainer}>
+        <Image
+          src="https://media.istockphoto.com/id/1334926091/photo/roof-of-new-house-with-shingles-roof-tiles-and-ventilation-window.jpg?s=2048x2048&w=is&k=20&c=N9WMvRaYjmgo3LUF4UelcLmxCH1hj7RxMZK5ER8LPx8="
+          className="h-full w-full"
+          alt="Your Image"
+          width={500}
+          height={200}
+        />
+      </div>
+      <Tagline />
       <div className="absolute top-4 right-20 h-50 w-1/4  m-2 py-5 flex items-center justify-center">
         <div className="p-4  h-full w-full bg-white opacity-95">
           <RoofingForm />
---
+++ src/components/Tagline.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Tagline = () => {
+  return <h1>Tagline</h1>;
+};
+
+export default Tagline;
